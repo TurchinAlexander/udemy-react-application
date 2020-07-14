15e0ba545efebc025361c8e9ed9f22bfaaa4084f
Alexander <AlexanderTurchin1@gmail.com>
2020-07-14 18:53:20 +0300

    Update the structure and create events on posts.

--- src/components/post-list/post-list.js
+++ src/components/post-list/post-list.js
@@ -4,12 +4,20 @@ import PostListItem from '../post-list-item';
 
 import './post-list.css';
 
-const PostList = () => {
+const PostList = ({posts}) => {    
+    const elements = posts.map((post) => {
+        const {id, ...postProps} = post;
+
+        return (
+            <li key={id} className="list-group-item" >
+                <PostListItem {...postProps}/>
+            </li>
+        );
+    });
+    
     return (
         <ul className="app-list list-group">
-            <PostListItem/>
-            <PostListItem/>
-            <PostListItem/>
+            {elements}
         </ul>
     );
 };
