83d4b3cd27160c3169c50d74a5463c1b67feb0d9
Alexander <AlexanderTurchin1@gmail.com>
2020-07-14 23:45:22 +0300

    Create methods and throwing them inside of the DOM tree.

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -8,26 +8,76 @@ import PostAddForm from '../post-add-form';
 
 import './app.css';
 
-const App = () => {
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            data : [
+                {label: 'Going to learn React', important: true, id: 1},
+                {label: 'That is so good', important: false, id: 2},
+                {label: 'I need a break', important: false, id: 3},
+            ]
+        };
 
-    const data = [
-        {label: 'Going to learn React', important: true, id: 'dsjfjf'},
-        {label: 'Going to learn React', important: true, id: 'qwqw'},
-        {label: 'That is so good', important: false, id: 'werr'},
-        {label: 'I need a break', important: false, id: 'fdfdf'},
-    ];
+        this.deleteItem = this.deleteItem.bind(this);
+        this.addItem = this.addItem.bind(this);
 
-    return (
-        <div className="app">
-            <AppHeader/>
-            <div className="search-panel d-flex">
-                <SearchPanel/>
-                <PostStatusFilter/>
+        this.maxId = 4;
+    }
+
+    deleteItem(id) {
+        this.setState(({data}) => {
+            const index = data.findIndex(elem => elem.id === id);
+            const newArray = data.slice();
+
+            newArray.splice(index, 1);
+
+            return {
+                data: newArray
+            };
+        });
+    }
+
+    addItem(body) {
+        const newItem = {
+            label: body,
+            important: false,
+            id: this.maxId++
+        };
+
+        this.setState(({data}) => {
+            // const newArray = data.slice();
+
+            // newArray.push(newItem);
+
+            const newArray = [...data, newItem];
+
+            return {
+                data: newArray
+            };
+        });
+    }
+
+    render() {
+        const {data} = this.state;
+
+        return (
+            <div className="app">
+                <AppHeader/>
+                <div className="search-panel d-flex">
+                    <SearchPanel/>
+                    <PostStatusFilter/>
+                </div>
+                <PostList 
+                    posts={data}
+                    onDelete={this.deleteItem}
+                />
+                <PostAddForm
+                    onAdd={this.addItem}    
+                />
             </div>
-            <PostList posts={data}/>
-            <PostAddForm/>
-        </div>
-    );
+        );
+    }
 };
 
 export default App;
--- src/components/post-list/post-list.js
+++ src/components/post-list/post-list.js
@@ -4,13 +4,16 @@ import PostListItem from '../post-list-item';
 
 import './post-list.css';
 
-const PostList = ({posts}) => {    
+const PostList = ({posts, onDelete}) => {    
     const elements = posts.map((post) => {
         const {id, ...postProps} = post;
 
         return (
             <li key={id} className="list-group-item" >
-                <PostListItem {...postProps}/>
+                <PostListItem 
+                    {...postProps}
+                    onDelete={() => onDelete(id)}
+                />
             </li>
         );
     });
